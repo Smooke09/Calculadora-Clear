789b15590594a53608060a807af5e147a9ca0a5c
Pedro Lucas <pedrolucas1014zz@gmail.com>
2022-05-04 11:39:56 -0300

    applying event using splice

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -27,16 +27,31 @@ class CalcController {
 
     }
 
+    // Metodo
+    addEventListenerAll(element, events, fn) {
+
+        events.split(' ').forEach(event => {
+
+            element.addEventListener(event, fn, false);
+        })
+    }
+
+
     initButtonsEVents() {
 
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
         buttons.forEach((btn, index) => {
 
-            btn.addEventListener('click', e => {
+            this.addEventListenerAll(btn, 'click drag', e => {
 
                 console.log(btn.className.baseVal.replace("btn-", ""));
             });
+
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+
+                btn.style.cursor = "pointer";
+            })
         });
     }
 
