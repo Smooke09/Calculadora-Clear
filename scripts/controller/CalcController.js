4167a534edf219d90b67567a4d5a970ecbf413c2
Pedro Lucas <pedrolucas1014zz@gmail.com>
2022-05-04 10:44:03 -0300

    Date and Hour

---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,92 @@
+class CalcController {
+
+
+    constructor() {
+
+        // atributo
+        this._locale = 'pt-BR'
+        // pegando Valor 
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+
+        this._currentDate;
+        this.initialize();
+
+    }
+
+    // metodo principal ao iniciar acontecer
+    initialize() {
+
+        this.setDisplayDateTime();
+
+        setInterval(() => {
+
+            this.setDisplayDateTime();
+        }, 1000)
+    }
+
+    // Metodo
+    setDisplayDateTime() {
+
+
+
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+            day: '2-digit',
+            month: "long",
+            year: "numeric"
+        })
+
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
+
+    // adicionando no DOM
+    get displayTime() {
+
+        return this._timeEl.innerHTML;
+
+    }
+
+    set displayTime(value) {
+
+        return this._timeEl.innerHTML = value;
+
+    }
+
+    get displayDate() {
+
+        return this._dateEl.innerHTML;
+
+    }
+
+    set displayDate(value) {
+
+        return this._dateEl.innerHTML = value;
+
+    }
+
+    get displayCalc() {
+
+        return this._displayCalcEl.innerHTML;
+
+    }
+
+    set displayCalc(value) {
+
+        this._displayCalcEl.innerHTML = value;
+
+    }
+
+    get currentDate() {
+
+        return new Date();
+
+    }
+
+    set currentDate(value) {
+
+        this._currentDate = value;
+
+    }
+
+}
